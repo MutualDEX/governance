ce14b49f21d7618fd1ad5434a155b0c5149a8fc2
MutualDEX <35112540+MutualDEX@users.noreply.github.com>
2021-01-04 13:24:57 -0500

    Update fixtures.ts

--- test/fixtures.ts
+++ test/fixtures.ts
@@ -2,16 +2,16 @@ import chai, { expect } from 'chai'
 import { Contract, Wallet, providers } from 'ethers'
 import { solidity, deployContract } from 'ethereum-waffle'
 
-import Elt from '../build/Elt.json'
-import EliteTimelock from '../build/EliteTimelock.json'
-import EliteGovernorAlpha from '../build/EliteGovernorAlpha.json'
+import Bgsp from '../build/Bgsp.json'
+import BigTimelock from '../build/BigTimelock.json'
+import BigGovernorAlpha from '../build/BigGovernorAlpha.json'
 
 import { DELAY } from './utils'
 
 chai.use(solidity)
 
 interface GovernanceFixture {
-  elt: Contract
+  bgsp: Contract
   timelock: Contract
   governorAlpha: Contract
 }
@@ -23,16 +23,16 @@ export async function governanceFixture(
   // deploy ELT, sending the total supply to the deployer
   const { timestamp: now } = await provider.getBlock('latest')
   const timelockAddress = Contract.getContractAddress({ from: wallet.address, nonce: 1 })
-  const elt = await deployContract(wallet, Elt, [wallet.address, timelockAddress, now + 60 * 60])
+  const bgsp = await deployContract(wallet, Bgsp, [wallet.address, timelockAddress, now + 60 * 60])
 
   // deploy timelock, controlled by what will be the governor
   const governorAlphaAddress = Contract.getContractAddress({ from: wallet.address, nonce: 2 })
-  const timelock = await deployContract(wallet, EliteTimelock, [governorAlphaAddress, DELAY])
+  const timelock = await deployContract(wallet, BigTimelock, [governorAlphaAddress, DELAY])
   expect(timelock.address).to.be.eq(timelockAddress)
 
   // deploy governorAlpha
-  const governorAlpha = await deployContract(wallet, EliteGovernorAlpha, [timelock.address, elt.address])
+  const governorAlpha = await deployContract(wallet, BigGovernorAlpha, [timelock.address, bgsp.address])
   expect(governorAlpha.address).to.be.eq(governorAlphaAddress)
 
-  return { elt, timelock, governorAlpha }
+  return { bgsp, timelock, governorAlpha }
 }
